1560416854eef2d86be08daba33ae3db961ddd9c
Manuel Hawlicek <manuel.hawlicek@gmail.com>
2023-01-02 12:50:36 +0100

    Initial Commit

---
+++ src/main/java/com/accenture/theincrediblesassignmentjpa/TheincrediblesAssignmentJpaApplication.java
@@ -0,0 +1,13 @@
+package com.accenture.theincrediblesassignmentjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheincrediblesAssignmentJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TheincrediblesAssignmentJpaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/accenture/theincrediblesassignmentjpa/TheincrediblesAssignmentJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.accenture.theincrediblesassignmentjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheincrediblesAssignmentJpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
